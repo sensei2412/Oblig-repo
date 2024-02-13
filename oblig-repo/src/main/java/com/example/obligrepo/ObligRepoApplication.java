bfbe821c107553e36d5930775821a9e8e2a6fd34
sensei2412 <144007494+sensei2412@users.noreply.github.com>
2024-02-13 23:30:55 +0100

    hei

---
+++ oblig-repo/src/main/java/com/example/obligrepo/ObligRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObligRepoApplication.class, args);
+    }
+
+}
---
+++ oblig-repo/src/test/java/com/example/obligrepo/ObligRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
