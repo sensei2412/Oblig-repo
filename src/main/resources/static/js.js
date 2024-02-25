d1ecfbe28fb58a2539f7b77c14b4bbb6acefc030
sensei2412 <144007494+sensei2412@users.noreply.github.com>
2024-02-25 11:24:37 +0100

    Update js.js

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -13,7 +13,9 @@ let epost = document.getElementById("epostEl");
 let epostRes = document.getElementById("epostResEl");
 let knapp = document.getElementById("knappEl");
 let slett = document.getElementById("slettEl");
+let slettKnapp = document.getElementById("slettKnappEl");
 let array = [];
+let funker = true;
 
 //bruker en addeventlistener for å lytte etter endring i selecten
 kinobiletter.addEventListener("change", function () {
@@ -31,47 +33,86 @@ knapp.addEventListener("click", function (){
         resultat.style.color = "red";
     } else {
 
-        if(antall.value == "") {
-            antallRes.innerHTML = "ingen antall";
+        if (antall.value == "" || antall.value >= 50) {
+            antallRes.innerHTML = "ingen eller mer enn 50 på antall";
             antallRes.style.color = "red";
             console.log('ingen antall');
+            funker = false;
         }
 
-        if(fornavn.value == "") {
+        if (fornavn.value == "") {
             console.log('ingen fornavn');
             fornavnRes.innerHTML = "ingen fornavn";
             fornavnRes.style.color = "red";
+            fornavn.innerHTML = "";
+            funker = false;
         }
 
-        if(etternavn.value == "") {
+        if (etternavn.value == "") {
             console.log('ingen etternavn');
             etternavnRes.innerHTML = "ingen etternavn";
             etternavnRes.style.color = "red";
+            etternavn.innerHTML = "";
+            funker = false;
         }
-        if(telfon.value == "") {
+        if (telfon.value == "") {
             console.log('ingen telefon');
             telefonRes.innerHTML = "ingen telefonnr";
             telefonRes.style.color = "red";
+            telfon.innerHTML = "";
+            funker = false;
         }
-        if(epost.value == "") {
+        if (epost.value == "") {
             console.log('ingen epost');
             epostRes.innerHTML = "ingen epost";
             epostRes.style.color = "red";
+            epost.innerHTML = "";
+            funker = false;
         }
+
+            // Bruk av valideringsfunksjoner:
+            // Validere telefonnummer
+            if (!validateTelefon(telfon.value)) {
+                console.log('ugyldig telefonnummer');
+                telefonRes.innerHTML = "Ugyldig telefonnummer";
+                telefonRes.style.color = "red";
+                funker = false;
+            } else {
+                console.log('gyldig telefonnummer');
+                telefonRes.innerHTML = "";
+            }
+
+            // Validere e-postadresse
+            if (!validateEpost(epost.value)) {
+                console.log('ugyldig e-postadresse');
+                epostRes.innerHTML = "Ugyldig e-postadresse";
+                epostRes.style.color = "red";
+                funker = false;
+            } else {
+                console.log('gyldig e-postadresse');
+                epostRes.innerHTML = "";
+            }
+            console.log(funker);
+            if(funker == false) {
+                sannhet();
+            }
+
+        }
+
 
         //bruker push komando for å dytte inn input verdiene
-        if(antall.value != "" && fornavn.value != "" && etternavn.value != "" && telfon.value != "" && epost.value != "") {
+        if(antall.value != "" && fornavn.value != "" && etternavn.value != "" && telfon.value != "" && epost.value != "" && funker == true) {
             array.push(etternavn.value);
-            array.push(antall.value);
             array.push(fornavn.value);
+            array.push(antall.value);
             array.push(telfon.value);
             array.push(epost.value);
-
-            slett.innerHTML = "<br/>" + " " + array;
+            slett.innerHTML = array;
+            sannhet();
         }
 
-    }
-})
+
+});
 
 // Regex mønster for å validere et norsk telefonnummer
 let telefonPattern = /^\d{8}$/;
@@ -89,36 +130,19 @@ function validateEpost(epost) {
     return epostPattern.test(epost);
 }
 
-// Bruk av valideringsfunksjoner:
-knapp.addEventListener("click", function () {
-    // Validere telefonnummer
-    if (!validateTelefon(telfon.value)) {
-        console.log('ugyldig telefonnummer');
-        telefonRes.innerHTML = "Ugyldig telefonnummer";
-        telefonRes.style.color = "red";
-    } else {
-        console.log('gyldig telefonnummer');
-        telefonRes.innerHTML = "";
-    }
-
-    // Validere e-postadresse
-    if (!validateEpost(epost.value)) {
-        console.log('ugyldig e-postadresse');
-        epostRes.innerHTML = "Ugyldig e-postadresse";
-        epostRes.style.color = "red";
-    } else {
-        console.log('gyldig e-postadresse');
-        epostRes.innerHTML = "";
-    }
-
-    // Fortsett med resten av valideringen og behandlingen av dataene...
-});
-
-
 //bare endrer array til en tom en
-slett.addEventListener("click", function () {
+slettKnapp.addEventListener("click", function () {
     array = [];
+    slett.innerHTML = array;
 })
+
+function sannhet () {
+    etternavn.value = "";
+    fornavn.value = "";
+    antall.value = "";
+    telfon.value = "";
+    epost.value = "";
+}
 
 
 
